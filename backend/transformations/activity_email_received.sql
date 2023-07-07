149e9a76ba430332530e2e0b0c4021c75d4ca945
astrosinformation <135932548+astrosinformation@users.noreply.github.com>
2023-07-06 21:16:04 -0400

    Update activity_email_received.sql

--- backend/transformations/activity_email_received.sql
+++ backend/transformations/activity_email_received.sql
@@ -1,1 +1,19 @@
-
+INSERT INTO "_CEL_SF_CASE_ACTIVITIES"(
+	"_CASE_KEY",
+	"ACTIVITY_DE",
+	"ACTIVITY_EN",
+	"EVENTTIME",
+	"_SORTING"
+)
+SELECT 
+	"Case"."Id" as "_CASE_KEY",
+	'Empfange E-Mail' as "ACTIVITY_DE",
+	'Receive Email' as "ACTIVITY_EN",
+	"EmailMessage"."CreatedDate" as "EVENTTIME",
+	20 as "_SORTING"
+FROM 
+	"Case"
+	JOIN "EmailMessage" ON
+		"Case"."Id" = "EmailMessage"."ParentId"
+WHERE
+	"EmailMessage"."Incoming" = true;
