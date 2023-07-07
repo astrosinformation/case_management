62ca7d4d6bf1075e26e9aaa6b603db5b8891fbc0
astrosinformation <135932548+astrosinformation@users.noreply.github.com>
2023-07-06 20:53:52 -0400

    Create change_type.sql

---
+++ backend/transformations/change_type.sql
@@ -0,0 +1,26 @@
+INSERT INTO "_CEL_SF_CASE_ACTIVITIES"(
+	"_CASE_KEY",
+	"ACTIVITY_DE",
+	"ACTIVITY_EN",
+	"EVENTTIME",
+	"_SORTING",
+	"CHANGED_FIELD",
+	"CHANGED_FROM",
+	"CHANGED_TO"
+)
+SELECT 
+	"Case"."Id" as "_CASE_KEY",
+	'Ändere Typ' as "ACTIVITY_DE",
+	'Change Type' as "ACTIVITY_EN",
+	"CaseHistory"."CreatedDate" as "EVENTTIME",
+	10 as "_SORTING",
+	"Field" as "CHANGED_FIELD",
+	"OldValue" as "CHANGED_FROM",
+	"NewValue" as "CHANGED_TO"
+FROM 
+	"Case"
+	JOIN "CaseHistory" ON
+		"Case"."Id" = "CaseHistory"."CaseId"
+WHERE
+	"Field" like 'Type'
+;
